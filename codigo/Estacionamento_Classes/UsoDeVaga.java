9276c5b91c6d0248e592edff12e0b1f35b3338d1
augustoandrade9 <67920085+augustoandrade9@users.noreply.github.com>
2023-10-04 12:02:20 -0300

    Update UsoDeVaga.java

--- codigo/Estacionamento_Classes/UsoDeVaga.java
+++ codigo/Estacionamento_Classes/UsoDeVaga.java
@@ -1,4 +1,5 @@
-
+import java.time.LocalDateTime;
+import java.time.Duration;
 
 public class UsoDeVaga {
 
@@ -11,15 +12,39 @@ public class UsoDeVaga {
 	private double valorPago;
 
 	public UsoDeVaga(Vaga vaga) {
-		
+		this.vaga = vaga;
+		this.entrada = LocalDateTime.now();
+		this.valorPago = 0.0;
+
+	
 	}
 
+	public int getMes(){
+		return entrada.getMonthValue();
+	}
+
+
 	public double sair() {
-		
+		this.saida = LocalDateTime.now();
+		Duration tempoEsta = Duration.between(entrada, saida);
+		long minutos = tempoEsta.toMinutes();
+		double aPagar = minutos * VALOR_FRACAO * FRACAO_USO;
+
+		if(aPagar > VALOR_MAXIMO){
+			this.valorPago = VALOR_MAXIMO;
+		}
+		else{
+			this.valorPago = aPagar;
+		}
+
+
+		vaga.disponivel();
+		return valorPago;
+
 	}
 
 	public double valorPago() {
-		
+		return valorPago;
 	}
 
 }
