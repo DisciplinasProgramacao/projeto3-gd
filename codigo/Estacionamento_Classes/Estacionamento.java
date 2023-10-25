bc04ed40189cda24c6e2b89427a8211eac00f008
PeterSilva05 <130841320+PeterSilva05@users.noreply.github.com>
2023-10-24 21:58:16 -0300

    Update Estacionamento.java

--- codigo/Estacionamento_Classes/Estacionamento.java
+++ codigo/Estacionamento_Classes/Estacionamento.java
@@ -10,7 +10,7 @@ public class Estacionamento {
     private int vagasPorFileira;
     private Map<String, Double> arrecadacaoPorPlaca;
 
-	public Estacionamento(String nome, int fileiras, int vagasPorFila) {
+    public Estacionamento(String nome, int fileiras, int vagasPorFila) {
         this.nome = nome;
         this.quantFileiras = fileiras;
         this.vagasPorFileira = vagasPorFila;
@@ -18,23 +18,24 @@ public class Estacionamento {
         this.vagas = new Vaga[0];
         this.arrecadacaoPorPlaca = new HashMap<>();
         gerarVagas();
+        criarCargaDados();
     }
 
-	public void addVeiculo(Veiculo veiculo, String idCli) {
+    public void addVeiculo(Veiculo veiculo, String idCli) {
         Cliente cliente = buscarClientePorId(idCli);
         if (cliente != null) {
             cliente.addVeiculo(veiculo);
         }
     }
 
-	public void addCliente(Cliente cliente) {
+    public void addCliente(Cliente cliente) {
         Cliente[] novosClientes = new Cliente[clientes.length + 1];
         System.arraycopy(clientes, 0, novosClientes, 0, clientes.length);
         novosClientes[clientes.length] = cliente;
         clientes = novosClientes;
     }
 
-	private void gerarVagas() {
+    private void gerarVagas() {
         char fila = 'A';
         int numeroVaga = 1;
         int totalVagas = quantFileiras * vagasPorFileira;
@@ -48,7 +49,53 @@ public class Estacionamento {
         }
     }
 
-	public void estacionar(String placa) {
+    private void criarCargaDados() {
+        // Criar 3 estacionamentos
+        Estacionamento estacionamento1 = new Estacionamento("Estacionamento 1", 5, 10);
+        Estacionamento estacionamento2 = new Estacionamento("Estacionamento 2", 4, 8);
+        Estacionamento estacionamento3 = new Estacionamento("Estacionamento 3", 6, 12);
+
+        // Criar 6 clientes com um veículo cada
+        Cliente cliente1 = new Cliente("Cliente 1");
+        Cliente cliente2 = new Cliente("Cliente 2");
+        Cliente cliente3 = new Cliente("Cliente 3");
+        Cliente cliente4 = new Cliente("Cliente 4");
+        Cliente cliente5 = new Cliente("Cliente 5");
+        Cliente cliente6 = new Cliente("Cliente 6");
+
+        Veiculo veiculo1 = new Veiculo("ABC1234");
+        Veiculo veiculo2 = new Veiculo("DEF5678");
+        Veiculo veiculo3 = new Veiculo("GHI9101");
+        Veiculo veiculo4 = new Veiculo("JKL2345");
+        Veiculo veiculo5 = new Veiculo("MNO6789");
+        Veiculo veiculo6 = new Veiculo("PQR1234");
+
+        cliente1.addVeiculo(veiculo1);
+        cliente2.addVeiculo(veiculo2);
+        cliente3.addVeiculo(veiculo3);
+        cliente4.addVeiculo(veiculo4);
+        cliente5.addVeiculo(veiculo5);
+        cliente6.addVeiculo(veiculo6);
+
+        addCliente(cliente1);
+        addCliente(cliente2);
+        addCliente(cliente3);
+        addCliente(cliente4);
+        addCliente(cliente5);
+        addCliente(cliente6);
+
+        // Distribuir 50 usos de vagas para clientes e estacionamentos
+        for (int i = 0; i < 50; i++) {
+            estacionar(veiculo1.getPlaca());
+            estacionar(veiculo2.getPlaca());
+            estacionar(veiculo3.getPlaca());
+            estacionar(veiculo4.getPlaca());
+            estacionar(veiculo5.getPlaca());
+            estacionar(veiculo6.getPlaca());
+        }
+    }
+
+    public void estacionar(String placa) {
         Vaga vagaLivre = encontrarVagaLivre();
         if (vagaLivre != null) {
             vagaLivre.ocupar();
@@ -56,7 +103,7 @@ public class Estacionamento {
         }
     }
 
-	public double sair(String placa) {
+    public double sair(String placa) {
         Vaga vagaOcupada = encontrarVagaOcupada(placa);
         if (vagaOcupada != null) {
             vagaOcupada.liberar();
@@ -67,7 +114,7 @@ public class Estacionamento {
         return 0.0;
     }
 
-	public double totalArrecadado() {
+    public double totalArrecadado() {
         double total = 0.0;
         for (double valor : arrecadacaoPorPlaca.values()) {
             total += valor;
@@ -75,7 +122,7 @@ public class Estacionamento {
         return total;
     }
 
-	public double arrecadacaoNoMes(int mes) {
+    public double arrecadacaoNoMes(int mes) {
         double totalMes = 0.0;
         for (Map.Entry<String, Double> entry : arrecadacaoPorPlaca.entrySet()) {
             String chave = entry.getKey();
@@ -87,16 +134,46 @@ public class Estacionamento {
         return totalMes;
     }
 
-	public double valorMedioPorUso() {
+    public double valorMedioPorUso() {
         if (arrecadacaoPorPlaca.size() == 0) {
             return 0.0;
         }
         return totalArrecadado() / arrecadacaoPorPlaca.size();
     }
 
-	public String top5Clientes(int mes) {
+    public String top5Clientes(int mes) {
         // Falta colocar a implementacao
         return "Top 5 clientes do mês " + mes;
     }
 
-}
+    private Cliente buscarClientePorId(String id) {
+        for (Cliente cliente : clientes) {
+            if (cliente.getId().equals(id)) {
+                return cliente;
+            }
+        }
+        return null;
+    }
+
+    private Vaga encontrarVagaLivre() {
+        for (Vaga vaga : vagas) {
+            if (!vaga.isOcupada()) {
+                return vaga;
+            }
+        }
+        return null;
+    }
+
+    private Vaga encontrarVagaOcupada(String placa) {
+        for (Vaga vaga : vagas) {
+            if (vaga.getPlacaEstacionada() != null && vaga.getPlacaEstacionada().equals(placa)) {
+                return vaga;
+            }
+        }
+        return null;
+    }
+
+    private double calcularValorPago(String placa) {
+        // Lógica de cálculo de valor a ser pago por uso da vaga
+        return 10.0; // Exemplo de valor
+
