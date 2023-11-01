f12eb70c0fef11118f3ace7315ac50dcf4b9fdd7
augustoandrade9 <67920085+augustoandrade9@users.noreply.github.com>
2023-11-01 11:33:53 -0300

    Update App.java

--- codigo/Estacionamento_Classes/App.java
+++ codigo/Estacionamento_Classes/App.java
@@ -27,108 +27,20 @@ public class App {
             escolha = scanner.nextInt();
             int esc = 0;
             switch (escolha) {
-
                 case 1:
-                    if(estac.length == 3){System.out.println("NUMERO MAXIMO ALCANCADO !!");}
-                    else{
-                        scanner.nextLine();
-                        System.out.println("Entre com o nome estacionamento: ");
-                        String nome = scanner.nextLine();
-                        System.out.println("Entre com o numero de fileiras: ");
-                        scanner.nextLine();
-                        int fileiras = scanner.nextInt();
-                        System.out.println("Entre com o nome vagas por fileiras: ");
-                        int vagas = scanner.nextInt();
-                        if (estac == null) {
-                            estac = new Estacionamento[1];
-                            estac[0] = new Estacionamento(nome, fileiras, vagas);
-                        } else {
-                            Estacionamento[] newEstac = new Estacionamento[estac.length + 1];
-                            for (int i = 0; i < estac.length; i++) {
-                                newEstac[i] = estac[i];
-                            }
-                            newEstac[estac.length] = new Estacionamento(nome, fileiras, vagas);
-                            estac = newEstac;
-                        }}
-
+                    criarEstacionamento(estac);
                     break;
                 case 2:
-                    int esc2 = 0;
-                    System.out.println("Selecione o estacionamento:");
-                    for (int i =0; i < estac.length;i++)
-                        System.out.println(i+1 +" "+ estac[i].getNome());
-                    esc = scanner.nextInt()-1;
-                    System.out.println("Registrar novo cliente. Escolha a opção:");
-                    System.out.println("1 para Cliente normal.");
-                    System.out.println("2 para Anônimo.");
-                    esc2 = scanner.nextInt();
-                    scanner.nextLine();
-
-                    if (esc2 == 1) {
-                        System.out.print("Digite o nome do cliente: ");
-                        String nomeCliente = scanner.nextLine();
-                        Cliente novoCliente = new Cliente(nomeCliente);
-                        estac[esc].addCliente(novoCliente);
-                    } else if (esc2 == 2) {
-                        Cliente novoCliente = new Cliente();
-                        estac[esc].addCliente(novoCliente);
-                    } else {
-                        System.out.println("Opção inválida. Tente novamente.");
-                    }
+                    cadastrarCliente(estac);
                     break;
-
                 case 3:
-                    String codigCli;
-                    System.out.println("Selecione o estacionamento:");
-                    for (int i =0; i < estac.length;i++)
-                        System.out.println(i+1 +" "+ estac[i].getNome());
-                    esc = scanner.nextInt()-1;
-                    Cliente[] cli = estac[esc].getClientes();
-                    System.out.flush();
-                    System.out.println("Insira o código do Cliente dono do veículo:");
-                    for (int i = 0; i < cli.length; i++){
-                        System.out.println(cli[i].getNome() + " Codigo: "+ cli[i].getId());
-                    }
-                    codigCli = scanner.nextLine();
-
-                        System.out.println("Insira a placa do veículo:");
-                        String placa = scanner.nextLine();
-
-                        System.out.println("Insira o modelo do veículo:");
-                        String modelo = scanner.nextLine();
-
-                        Veiculo novoVeiculo = new Veiculo(placa, modelo);
-
-                        estac[esc].addVeiculo(novoVeiculo, codigCli);
-
+                    cadastrarVeiculo(estac);
                     break;
                 case 4:
-                    String placaEstacionar;
-                    System.out.println("Selecione o estacionamento:");
-                    for (int i =0; i < estac.length;i++)
-                        System.out.println(i+1 +" "+ estac[i].getNome());
-                    esc = scanner.nextInt()-1;
-                    System.out.println("Estacionar Veículo \nInsira a PLACA: ");
-                    placaEstacionar = scanner.nextLine();
-
-                    estac[esc].estacionar(placaEstacionar);
-
-
+                    estacionarVeiculo(estac);
                     break;
                 case 5:
-                    String placaSaida;
-                    Double valor;
-                    System.out.println("Selecione o estacionamento:");
-                    for (int i =0; i < estac.length;i++)
-                        System.out.println(i+1 +" "+ estac[i].getNome());
-                    esc = scanner.nextInt()-1;
-
-                    System.out.println("Insira a placa do carro:");
-                    placaSaida = scanner.nextLine();
-
-                    valor = estac[esc].sair(placaSaida);
-
-                    System.out.println("TOTAL A PAGAR: "+ valor);
+                    sairVeiculo(estac);
                     break;
                 case 6:
                     System.out.println("Saindo do programa.");
@@ -140,5 +52,111 @@ public class App {
         }
         scanner.close();
     }
+
+     public static void criarEstacionamento(Estacionamento[] estac){
+        if(estac.length == 3){System.out.println("NUMERO MAXIMO ALCANCADO !!");}
+            else{
+                scanner.nextLine();
+                System.out.println("Entre com o nome estacionamento: ");
+                String nome = scanner.nextLine();
+                System.out.println("Entre com o numero de fileiras: ");
+                scanner.nextLine();
+                int fileiras = scanner.nextInt();
+                System.out.println("Entre com o nome vagas por fileiras: ");
+                int vagas = scanner.nextInt();
+                if (estac == null) {
+                    estac = new Estacionamento[1];
+                    estac[0] = new Estacionamento(nome, fileiras, vagas);
+                } else {
+                    Estacionamento[] newEstac = new Estacionamento[estac.length + 1];
+                    for (int i = 0; i < estac.length; i++) {
+                        newEstac[i] = estac[i];
+                    }
+                    newEstac[estac.length] = new Estacionamento(nome, fileiras, vagas);
+                        estac = newEstac;
+                    }}
+
+    }
+
+    public static void cadastrarCliente(Estacionamento[] estac){
+        int esc2 = 0;
+            System.out.println("Selecione o estacionamento:");
+            for (int i =0; i < estac.length;i++)
+                System.out.println(i+1 +" "+ estac[i].getNome());
+                esc = scanner.nextInt()-1;
+                System.out.println("Registrar novo cliente. Escolha a opção:");
+                System.out.println("1 para Cliente normal.");
+                System.out.println("2 para Anônimo.");
+                esc2 = scanner.nextInt();
+                scanner.nextLine();
+
+                if (esc2 == 1) {
+                    System.out.print("Digite o nome do cliente: ");
+                    String nomeCliente = scanner.nextLine();
+                    Cliente novoCliente = new Cliente(nomeCliente);
+                    estac[esc].addCliente(novoCliente);
+                } else if (esc2 == 2) {
+                    Cliente novoCliente = new Cliente();
+                    estac[esc].addCliente(novoCliente);
+                } else {
+                    System.out.println("Opção inválida. Tente novamente.");
+                }
+    }
+
+    public static void cadastrarVeiculo(Estacionamento[] estac){
+        String codigCli;
+            System.out.println("Selecione o estacionamento:");
+            for (int i =0; i < estac.length;i++)
+                System.out.println(i+1 +" "+ estac[i].getNome());
+                esc = scanner.nextInt()-1;
+                Cliente[] cli = estac[esc].getClientes();
+                System.out.flush();
+                System.out.println("Insira o código do Cliente dono do veículo:");
+            for (int i = 0; i < cli.length; i++){
+                System.out.println(cli[i].getNome() + " Codigo: "+ cli[i].getId());
+            }
+            codigCli = scanner.nextLine();
+
+                System.out.println("Insira a placa do veículo:");
+                String placa = scanner.nextLine();
+
+                System.out.println("Insira o modelo do veículo:");
+                String modelo = scanner.nextLine();
+
+                Veiculo novoVeiculo = new Veiculo(placa, modelo);
+
+                estac[esc].addVeiculo(novoVeiculo, codigCli);
+
+    }
+
+    public static void estacionarVeiculo(Estacionamento[] estac){
+        String placaEstacionar;
+            System.out.println("Selecione o estacionamento:");
+            for (int i =0; i < estac.length;i++)
+                System.out.println(i+1 +" "+ estac[i].getNome());
+                esc = scanner.nextInt()-1;
+                System.out.println("Estacionar Veículo \nInsira a PLACA: ");
+                placaEstacionar = scanner.nextLine();
+
+                estac[esc].estacionar(placaEstacionar);
+    }
+
+    public static void sairVeiculo(Estacionamento[] estac){
+        String placaSaida;
+            Double valor;
+            System.out.println("Selecione o estacionamento:");
+            for (int i =0; i < estac.length;i++)
+                System.out.println(i+1 +" "+ estac[i].getNome());
+                esc = scanner.nextInt()-1;
+
+                System.out.println("Insira a placa do carro:");
+                placaSaida = scanner.nextLine();
+
+                valor = estac[esc].sair(placaSaida);
+
+                System.out.println("TOTAL A PAGAR: "+ valor);
+    }
+
+
 
 }
