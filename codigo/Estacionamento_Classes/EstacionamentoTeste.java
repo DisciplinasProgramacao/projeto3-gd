3647d055436f9c1a817f05c5e367581f4ac7f355
Renato Ramalho <113562271+Renatojunior2022@users.noreply.github.com>
2023-11-01 10:34:09 -0300

    Update EstacionamentoTeste.java

--- codigo/Estacionamento_Classes/EstacionamentoTeste.java
+++ codigo/Estacionamento_Classes/EstacionamentoTeste.java
@@ -1,40 +1,113 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class EstacionamentoTeste {
 
-    public static void main(String[] args) {
-        
-        Estacionamento estacionamento1 = new Estacionamento("Estacionamento A", 3, 4);
+    private Estacionamento estacionamento;
 
-        
+
+    @BeforeEach
+    public void setUp() {
+        estacionamento = new Estacionamento("Estacionamento A", 3, 4);
+    }
+
+    @Test
+    public void testAddCliente() {
         Cliente cliente1 = new Cliente("123", "John Doe", null);
-        estacionamento1.addCliente(cliente1);
+        estacionamento.addCliente(cliente1);
 
-        
+        Cliente[] clientes = estacionamento.getClientes();
+        assertEquals(1, clientes.length);
+        assertEquals(cliente1, clientes[0]);
+    }
+
+    @Test
+    public void testAddVeiculo() {
+        Cliente cliente1 = new Cliente("123", "John Doe", null);
         Veiculo veiculo1 = new Veiculo("ABC123", "Car");
-        estacionamento1.addVeiculo(veiculo1, "123");
+        estacionamento.addCliente(cliente1);
 
-        
-        estacionamento1.estacionar("ABC123");
+        estacionamento.addVeiculo(veiculo1, "123");
+        Veiculo[] veiculos = cliente1.getVeiculo();
+        assertEquals(1, veiculos.length);
+        assertEquals(veiculo1, veiculos[0]);
+    }
 
-        
-        double valorArrecadado = estacionamento1.sair("ABC123");
-        System.out.println("Valor arrecadado: " + valorArrecadado);
+    /**
+     * 
+     */
+    @Test
+    public void testEstacionar() {
+        Cliente cliente1 = new Cliente("123", "John Doe", null);
+        Veiculo veiculo1 = new Veiculo("ABC123", "Car");
+        estacionamento.addCliente(cliente1);
+        estacionamento.addVeiculo(veiculo1, "123");
 
-       
-        double totalArrecadado = estacionamento1.totalArrecadado();
-        System.out.println("Total arrecadado: " + totalArrecadado);
+        estacionamento.estacionar("ABC123");
+        Vaga[] vagas = estacionamento.getVagas();
+        boolean vagaOcupada = false;
+        for (Vaga vaga : vagas) {
+            if (!vaga.disponivel()) {
+                vagaOcupada = true;
+                break;
+            }
+        }
+        assertTrue(vagaOcupada);
+    }
 
-        
-        double arrecadacaoNoMes = estacionamento1.arrecadacaoNoMes(10);
-        System.out.println("Arrecadacao no mes: " + arrecadacaoNoMes);
+    @Test
+    public void testSair() {
+        Cliente cliente1 = new Cliente("123", "John Doe", null);
+        Veiculo veiculo1 = new Veiculo("ABC123", "Car");
+        estacionamento.addCliente(cliente1);
+        estacionamento.addVeiculo(veiculo1, "123");
 
-        
-        double valorMedioPorUso = estacionamento1.valorMedioPorUso();
-        System.out.println("Valor medio por uso: " + valorMedioPorUso);
+        estacionamento.estacionar("ABC123");
+        double valorArrecadado = estacionamento.sair("ABC123");
 
-        
-        String top5Clientes = estacionamento1.top5Clientes(10);
-        System.out.println(top5Clientes);
+        assertEquals(0.0, valorArrecadado); // Assumindo que o valor retornado ao sair é 0.0 para o exemplo
+    }
 
-        
+    @Test
+    public void testTotalArrecadado() {
+        Cliente cliente1 = new Cliente("123", "John Doe", null);
+        Veiculo veiculo1 = new Veiculo("ABC123", "Car");
+        estacionamento.addCliente(cliente1);
+        estacionamento.addVeiculo(veiculo1, "123");
+
+        estacionamento.estacionar("ABC123");
+        double totalArrecadado = estacionamento.totalArrecadado();
+
+        assertEquals(0.0, totalArrecadado); // Assumindo que o valor arrecadado é 0.0 para o exemplo
+    }
+
+    @Test
+    public void testArrecadacaoNoMes() {
+        Cliente cliente1 = new Cliente("123", "John Doe", null);
+        Veiculo veiculo1 = new Veiculo("ABC123", "Car");
+        estacionamento.addCliente(cliente1);
+        estacionamento.addVeiculo(veiculo1, "123");
+
+        estacionamento.estacionar("ABC123");
+        estacionamento.sair("ABC123");
+
+        double arrecadacao = estacionamento.arrecadacaoNoMes(10); // Supondo que o mês seja outubro (10)
+        assertEquals(0.0, arrecadacao); // Como o veículo ficou apenas um dia, a arrecadação deve ser 0.0
+    }
+
+    @Test
+    public void testValorMedioPorUso() {
+        Cliente cliente1 = new Cliente("123", "John Doe", null);
+        Veiculo veiculo1 = new Veiculo("ABC123", "Car");
+        estacionamento.addCliente(cliente1);
+        estacionamento.addVeiculo(veiculo1, "123");
+
+        estacionamento.estacionar("ABC123");
+        estacionamento.sair("ABC123");
+
+        double valorMedio = estacionamento.valorMedioPorUso();
+        assertEquals(0.0, valorMedio); // Como só houve um uso, o valor médio deve ser 0.0
     }
 }
