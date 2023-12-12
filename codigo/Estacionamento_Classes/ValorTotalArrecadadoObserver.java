4291acc842599b0159b629aef7810a8267e171c2
Renato Ramalho <113562271+Renatojunior2022@users.noreply.github.com>
2023-12-12 20:35:11 -0300

    Update ValorTotalArrecadadoObserver.java

--- codigo/Estacionamento_Classes/ValorTotalArrecadadoObserver.java
+++ codigo/Estacionamento_Classes/ValorTotalArrecadadoObserver.java
@@ -4,24 +4,32 @@ import java.util.Observer;
 public class ValorTotalArrecadadoObserver implements Observer {
 
     private double valorTotalArrecadado;
+    private Estacionamento estacionamento;
 
-    public ValorTotalArrecadadoObserver() {
+    public ValorTotalArrecadadoObserver(Estacionamento estacionamento) {
+        this.estacionamento = estacionamento;
         this.valorTotalArrecadado = 0.0;
+        estacionamento.addObserver(this);
+        calcularValorTotalArrecadado();
     }
 
     @Override
-    public void update(Observable o, Object arg) {
-        if (o instanceof Estacionamento && arg instanceof Double) {
-            Estacionamento estacionamento = (Estacionamento) o;
-            double novoValor = (Double) arg;
-            
-            this.valorTotalArrecadado = novoValor;
-           
-            System.out.println("Novo valor total arrecadado: " + novoValor);
+    public void update(Observable observable, Object arg) {
+        if (observable instanceof Estacionamento) {
+            Estacionamento est = (Estacionamento) observable;
+            double novoValor = est.calcularValorTotal(est);
+            if (this.valorTotalArrecadado < novoValor) {
+                System.out.println("Valor total arrecadado aumentou de " + this.valorTotalArrecadado + " para " + novoValor);
+                this.valorTotalArrecadado = novoValor;
+            }
         }
     }
 
     public double getValorTotalArrecadado() {
         return valorTotalArrecadado;
+    }
+
+    public void pararObservacao() {
+        estacionamento.deleteObserver(this);
     }
 }
