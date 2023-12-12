d6b164330d4515b99711db1684bc37d016dc29de
Davi Andrade <103124803+dmelo10@users.noreply.github.com>
2023-12-12 17:40:48 -0300

    Update Top5Clientes.java

--- codigo/Estacionamento_Classes/Top5Clientes.java
+++ codigo/Estacionamento_Classes/Top5Clientes.java
@@ -1,3 +1,7 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Top5Clientes implements Observer {
 
     private List<Cliente> clientes;
@@ -8,7 +12,7 @@ public class Top5Clientes implements Observer {
         this.clientes = clientes;
         this.mes = mes;
         this.dadosObservados = dadosObservados;
-        dadosObservados.adicionarObservador(this);  
+        dadosObservados.adicionarObservador(this);
     }
 
     @Override
@@ -17,16 +21,25 @@ public class Top5Clientes implements Observer {
     }
 
     private void calcularTop5Clientes() {
-        List<Cliente> top5Clientes = clientes.stream()
-                .filter(cliente -> cliente.arrecadadoNoMes(mes) > 0)
-                .sorted(Comparator.comparing(Cliente::arrecadadoNoMes).reversed())
-                .limit(5)
-                .collect(Collectors.toList());
+        if (clientes != null) {
+            List<Cliente> top5Clientes = clientes.stream()
+                    .filter(cliente -> cliente.arrecadadoTotal() > 0)
+                    .sorted(criarComparadorTop5Clientes())
+                    .limit(5)
+                    .collect(Collectors.toList());
 
-        System.out.println("Top 5 clientes que mais geraram arrecadação no mês " + mes + ":");
-        for (int i = 0; i < top5Clientes.size(); i++) {
-            System.out.println("Cliente " + (i + 1) + ": " + top5Clientes.get(i).getNome() + " - " + top5Clientes.get(i).arrecadadoNoMes(mes));
+            System.out.println("Top 5 clientes que mais geraram arrecadação:");
+            for (int i = 0; i < top5Clientes.size(); i++) {
+                System.out.println("Cliente " + (i + 1) + ": " + top5Clientes.get(i).getNome() + " - " + top5Clientes.get(i).arrecadadoTotal());
+            }
         }
+    }
+
+    private Comparator<Cliente> criarComparadorTop5Clientes() {
+        return Comparator.<Cliente, Double>comparing(cliente -> cliente.arrecadadoTotal())
+                .thenComparing(Comparator.<Cliente, Double>comparing(cliente -> cliente.arrecadadoNoMes(mes))
+                        .reversed())
+                .reversed();
     }
 
     public void pararObservacao() {
